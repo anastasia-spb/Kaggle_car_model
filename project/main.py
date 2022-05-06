2760b61d5802745372382a34a60c199e28e04307
anastasia-spb <anastasia.vlasova6293@gmail.com>
2022-05-07 00:16:12 +0200

    Refactoring

--- project/main.py
+++ project/main.py
@@ -4,21 +4,19 @@ import os
 from torchvision import transforms
 from jpeg_dataset import JpgDataset
 from custom_transformations import PerImageNormalization
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
-from model import AlexCaptchaNet
-from torch.nn import CrossEntropyLoss
-from torch.optim import Adam
 import numpy as np
 import pandas as pd
+from model_launcher import ModelLauncher
 
+
+# Dataset parameters
 CLASS_NUM = 10
 IMG_SIZE = 227
 IMG_CHANNELS = 3
+# If True, then read raw dataset, transform and form Torch Dataset.
+# Otherwise, read torch dataset
 READ_DATASET = True
-BATCH_SIZE = 64
-LEARNING_RATE = 1e-4
-EPOCH_NUM = 50
+# Train model if True. Load model from file otherwise.
 TRAIN = False
 
 
@@ -28,7 +26,7 @@ TRAIN = False
 # It took around 100Gb of Swp
 def create_dataset(root_dir, labels_csv='', train=True):
     image_transformation = transforms.Compose([
-        transforms.ToPILImage(),  # Convert a tensor or an ndarray to PIL Image
+        transforms.ToPILImage(),  # Convert a tensor or a ndarray to PIL Image
         transforms.Resize((IMG_SIZE, IMG_SIZE)),
         transforms.ToTensor(),
         PerImageNormalization()])
@@ -44,6 +42,19 @@ def create_dataset(root_dir, labels_csv='', train=True):
         return JpgDataset(root_dir, labels_csv, image_transformation)
 
 
+def get_dataset():
+    # Load or reload and store dataset
+    dataset_name = 'avtovaz_dataset.pt'
+    if READ_DATASET:
+        dataset = create_dataset('../data/train', '../data/train.csv')
+        print("Dataset creation finished.")
+        # torch.save(torch_dataset.clone(), dataset_name)
+    else:
+        dataset = torch.load('avtovaz_dataset.pt')
+
+    return dataset
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Cuda maintenance
@@ -53,88 +64,22 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Torch device: ", device)
 
+    model_launcher = ModelLauncher(device)
+    dataset_for_submission = create_dataset('../data/test', '', False)
+
     if TRAIN:
-        # Load or reload and store dataset
-        dataset_name = 'avtovaz_dataset.pt'
-        if READ_DATASET:
-            torch_dataset = create_dataset('../data/train', '../data/train.csv')
-            print("Dataset creation finished.")
-            # torch.save(torch_dataset.clone(), dataset_name)
-        else:
-            torch_dataset = torch.load('avtovaz_dataset.pt')
-
-        # Split to validation and train sets
-        validation_set_size = int(0.2 * len(torch_dataset))
-        train_set_size = len(torch_dataset) - validation_set_size
-
-        train_subset, validation_subset = random_split(torch_dataset, [train_set_size, validation_set_size], generator=torch.Generator().manual_seed(42))
-        # Shuffle data both in train and validation sets
-        train_dl = DataLoader(train_subset, batch_size=BATCH_SIZE, shuffle=True)
-        val_dl = DataLoader(validation_subset, batch_size=BATCH_SIZE, shuffle=True)
-
-        model = AlexCaptchaNet()
-        model = model.to(device=device)  # send the model for training on either cuda or cpu
-
-        ## Loss and optimizer
-        learning_rate = LEARNING_RATE
-        criterion = CrossEntropyLoss()
-        optimizer = Adam(model.parameters(), lr=learning_rate)
-
-        for epoch in range(EPOCH_NUM):
-            loss_ep = 0
-            for batch_idx, (_, data, targets) in enumerate(train_dl):
-                data = data.to(device=device)
-                targets = targets.to(device=device)
-                ## Forward Pass
-                optimizer.zero_grad()
-                scores = model(data)
-                loss = criterion(scores, targets)
-                loss.backward()
-                optimizer.step()
-                loss_ep += loss.item()
-            print(f"Loss in epoch {epoch} :::: {loss_ep / len(train_dl)}")
-
-            with torch.no_grad():
-                num_correct = 0
-                num_samples = 0
-                for batch_idx, (_, data, targets) in enumerate(val_dl):
-                    data = data.to(device=device)
-                    targets = targets.to(device=device)
-                    ## Forward Pass
-                    scores = model(data)
-                    _, predictions = scores.max(1)
-                    num_correct += (predictions == targets).sum()
-                    num_samples += predictions.size(0)
-                print(
-                    f"Got {num_correct} / {num_samples} with accuracy {float(num_correct) / float(num_samples) * 100:.2f}"
-                )
-
-        # Save model
-        torch.save(model.state_dict(), './avtovaz_model.pth')
+        train_dataset = get_dataset()
+        model_launcher.train(train_dataset)
+        submission_results, img_names_column = model_launcher.predict(dataset_for_submission)
     else:
-        model = AlexCaptchaNet()
-        model = model.to(device=device)  # send the model for training on either cuda or cpu
-        model.load_state_dict(torch.load('avtovaz_model.pth'))
+        submission_results, img_names_column = model_launcher.predict(dataset_for_submission, './avtovaz_model.pth')
 
-        dataset_for_submission = create_dataset('../data/test', '', False)
-        submission_dl = DataLoader(dataset_for_submission, batch_size=BATCH_SIZE, shuffle=False)
+    # Write results into submission file
+    sample_submission = pd.read_csv('../data/sample-submission.csv')
+    sample_submission['Category'] = np.concatenate(submission_results)
+    sample_submission['Id'] = np.concatenate(img_names_column)
 
-        submission_resuls = []
-        img_names_column = []
-        for batch_idx, (img_names, data, _) in enumerate(submission_dl):
-            ## Forward Pass
-            scores = model(data.cuda())
-            softmax = torch.exp(scores).cpu()
-            prob = list(softmax.detach().numpy())
-            predictions = np.argmax(prob, axis=1)
-            submission_resuls.append(predictions)
-            img_names_column.append(img_names)
-
-        sample_submission = pd.read_csv('../data/sample-submission.csv')
-        sample_submission['Category'] = np.concatenate(submission_resuls)
-        sample_submission['Id'] = np.concatenate(img_names_column)
-
-        sample_submission.to_csv('avtovaz_submission2.csv', index=False)
+    sample_submission.to_csv('avtovaz_submission2.csv', index=False)
 
 
 
---
+++ project/model_launcher.py
@@ -0,0 +1,83 @@
+import torch
+from model import AlexCaptchaNet
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import numpy as np
+
+
+class ModelLauncher:
+    def __init__(self, device):
+        self.model = AlexCaptchaNet()
+        self.device = device
+        self.model = self.model.to(device=self.device)  # send the model for training on either cuda or cpu
+        # Initialize learning parameters
+        self.learning_rate = 1e-4
+        self.epochs = 50
+        self.criterion = CrossEntropyLoss()
+        self.optimizer = Adam(self.model.parameters(), lr=self.learning_rate)
+        self.batch_size = 64
+
+    def train(self, torch_dataset, save_model=False):
+        # Split to validation and train sets
+        validation_set_size = int(0.2 * len(torch_dataset))
+        train_set_size = len(torch_dataset) - validation_set_size
+
+        train_subset, validation_subset = random_split(torch_dataset, [train_set_size, validation_set_size],
+                                                       generator=torch.Generator().manual_seed(42))
+        # Shuffle data both in train and validation sets
+        train_dl = DataLoader(train_subset, batch_size=self.batch_size, shuffle=True)
+        val_dl = DataLoader(validation_subset, batch_size=self.batch_size, shuffle=True)
+
+        for epoch in range(self.epochs):
+            loss_ep = 0
+            for batch_idx, (_, data, targets) in enumerate(train_dl):
+                data = data.to(device=self.device)
+                targets = targets.to(device=self.device)
+                ## Forward Pass
+                self.optimizer.zero_grad()
+                scores = self.model(data)
+                loss = self.criterion(scores, targets)
+                loss.backward()
+                self.optimizer.step()
+                loss_ep += loss.item()
+            print(f"Loss in epoch {epoch} :::: {loss_ep / len(train_dl)}")
+
+            with torch.no_grad():
+                num_correct = 0
+                num_samples = 0
+                for batch_idx, (_, data, targets) in enumerate(val_dl):
+                    data = data.to(device=self.device)
+                    targets = targets.to(device=self.device)
+                    ## Forward Pass
+                    scores = self.model(data)
+                    _, predictions = scores.max(1)
+                    num_correct += (predictions == targets).sum()
+                    num_samples += predictions.size(0)
+                print(
+                    f"Got {num_correct} / {num_samples} with accuracy {float(num_correct) / float(num_samples) * 100:.2f}"
+                )
+
+            if save_model:
+                torch.save(self.model.state_dict(), './model_from_last_train.pth')
+
+    def predict(self, torch_dataset, model_from_file=''):
+        if model_from_file:
+            self.model.load_state_dict(torch.load(model_from_file))
+
+        submission_dl = DataLoader(torch_dataset, batch_size=self.batch_size, shuffle=False)
+
+        submission_results = []
+        img_names_column = []
+        for batch_idx, (img_names, data, _) in enumerate(submission_dl):
+            ## Forward Pass
+            scores = self.model(data.cuda())
+            softmax = torch.exp(scores).cpu()
+            prob = list(softmax.detach().numpy())
+            predictions = np.argmax(prob, axis=1)
+            submission_results.append(predictions)
+            img_names_column.append(img_names)
+
+        return submission_results, img_names_column
+
